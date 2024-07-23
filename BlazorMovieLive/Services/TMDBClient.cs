70bf55aaa901d647734cc5236c29218907a33afa
Obadia Gilbert <obadiagilbert@Obadias-MacBook-Pro.local>
2024-07-23 20:03:21 +0300

    created http service and appsettings.json for ApiKey

---
+++ BlazorMovieLive/Services/TMDBClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BlazorMovieLive.Services
+{
+	public class TMDBClient
+	{
+		private readonly HttpClient _httpClient;
+		public TMDBClient(HttpClient httpClient, IConfiguration config)
+		{
+			_httpClient = httpClient;
+			_httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+			_httpClient.DefaultRequestHeaders.Accept.Add(new("Applicaton/Json"));
+
+			string ApiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found!");
+			_httpClient.DefaultRequestHeaders.Authorization = new("Bearer",ApiKey);
+		}
+	}
+}
+
