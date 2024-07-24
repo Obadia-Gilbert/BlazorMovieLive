09a8d4292a9628824a5ab8cc2cd731649f8723cc
Obadia Gilbert <gilbertobadia830@gmail.com>
2024-07-24 20:41:30 +0300

    API TMDB implemented

--- BlazorMovieLive/Services/TMDBClient.cs
+++ BlazorMovieLive/Services/TMDBClient.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using System.Net.Http.Json;
+using BlazorMovieLive.Models;
+
 namespace BlazorMovieLive.Services
 {
-	public class TMDBClient
+    public class TMDBClient
 	{
 		private readonly HttpClient _httpClient;
 		public TMDBClient(HttpClient httpClient, IConfiguration config)
 		{
 			_httpClient = httpClient;
 			_httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
-			_httpClient.DefaultRequestHeaders.Accept.Add(new("Applicaton/Json"));
+			_httpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
 
-			string ApiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found!");
-			_httpClient.DefaultRequestHeaders.Authorization = new("Bearer",ApiKey);
+			string apiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found!");
+			_httpClient.DefaultRequestHeaders.Authorization = new("Bearer",apiKey);
+		}
+
+		public async Task<PopularMoviePagedResponse?> GetPopularMovieAsync()
+		{   
+			return await _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>("movie/popular");
 		}
 	}
 }
